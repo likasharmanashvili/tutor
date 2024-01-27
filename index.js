10d10554d6477c948e9414cac969545df2b08386
lika <l.sharmanashvili@ssu.edu.g>
2024-01-27 21:00:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,103 @@
+// const newdiv = document.createElement ("div");
+// newdiv.style.width = "200px"
+// newdiv.style.height = '200px'
+// newdiv.style.backgroundColor = 'red'
+
+// document.body.appendChild (newdiv)
+
+// newdiv.addEventListener ( "mouseenter", () => {
+//     newdiv.style.backgroundColor = 'green'
+// })
+// newdiv.addEventListener ( "mouseleave", () => {
+//     newdiv.style.backgroundColor = 'blue'
+// });
+
+
+
+
+//factroiali  mat ricxvamde kvela ricxvi gamravlebuli maf 5 is faqtoriali  = 1*2*3*4*5 = 120
+
+// function factorial (num){
+// if (num ===0  || num ===1){
+// return 1;
+// } else {
+// return factorial (num - 1) * num
+// }
+// }
+// console.log(factorial (5))
+
+
+
+//button increment decrement ppliusze miumatos gamoaklos minusze paagrafad gavatanot nuli
+
+
+
+// const plusbutton = document.createElement ("button")
+// const minusbutton = document.createElement ("button")
+// const number = document.createElement ("p")
+
+
+
+// number.textContent = 0;
+// plusbutton.textContent = "+";
+// minusbutton.textContent = "-";
+
+
+// document.body.appendChild  (number);
+// document.body.appendChild (plusbutton);
+// document.body.appendChild (minusbutton);
+
+// //exla fuqncia ro  kliklze gazardos an sheamciross
+
+
+// plusbutton.addEventListener("click", () =>  {
+//     number.textContent++;
+// }) 
+// minusbutton.addEventListener("click", () =>  {
+//     number.textContent--;
+//     if (number.textContent == 0) {
+//         alert ("that's enough")
+//     } else {
+//         number.textContent--;
+//     }
+// }) 
+
+
+
+//inputis validacia
+
+
+// const btn = document.querySelector (".btn");
+// const input =document.querySelector(".input");
+// const paragraph = document.querySelector (".paragraph");
+// paragraph.style.display = 'none'
+
+
+// btn.addEventListener ("click" ,() => {
+//     if (input.value.length === 0){
+//         paragraph.textContent = 'required'
+//         paragraph.style.color = 'red'
+//         paragraph.style.display = 'block'
+//     } else if (input.value.length < 5) 
+//     {
+//         paragraph.textContent = 'more  that 5'
+//     paragraph.style.color = 'red'
+//     paragraph.style.display = 'block' 
+//     } else {
+//         alert ("congratulations")
+//         input.value = ""
+//     }
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
